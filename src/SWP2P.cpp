b427ab7d10a6f073084bb17dc2a11f8d7b54e876
Sierrastdio <sierraliamg@gmail.com>
2026-09-01 23:11:06 +0900

    (SWP2P): add License(LGPL v3.0) 	modified:   src/SWP2P.cpp 	modified:   src/SWP2P.h 	modified:   src/SWP2PBuffer.h

--- src/SWP2P.cpp
+++ src/SWP2P.cpp
@@ -1,3 +1,20 @@
+/**********************************************************************************************************
+ * SWP2P
+ * Copyright (c) 2026 Sierrastdio
+ *
+ * SWP2P is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SWP2P is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ *            <http://www.gnu.org/licenses/>.
+ *********************************************************************************************************/
+
 #include "SWP2P.h"
 static_assert((SWP2P_FIFO_DEPTH & (SWP2P_FIFO_DEPTH - 1)) == 0, "SWP2P_FIFO_DEPTH must be a power of 2");
 
--- src/SWP2PBuffer.h
+++ src/SWP2PBuffer.h
@@ -1,3 +1,20 @@
+/**********************************************************************************************************
+ * SWP2P
+ * Copyright (c) 2026 Sierrastdio
+ *
+ * SWP2P is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SWP2P is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ *            <http://www.gnu.org/licenses/>.
+ *********************************************************************************************************/
+
 #ifndef SWP2P_BUFFER_H
 #define SWP2P_BUFFER_H
 #include <Arduino.h>
